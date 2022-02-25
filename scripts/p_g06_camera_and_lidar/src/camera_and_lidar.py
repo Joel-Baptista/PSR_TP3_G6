cb35e57558f0b1d1f078b770ccd04d7c977c02da
jorge <jorge.andre.silva.fernandes@gmail.com>
2022-02-25 15:56:59 +0000

    update on rgb limits

--- scripts/p_g06_camera_and_lidar/src/camera_and_lidar.py
+++ scripts/p_g06_camera_and_lidar/src/camera_and_lidar.py
@@ -18,19 +18,20 @@ camera_node = rospy.remap_name("blue1/camera/rgb/image_raw")
 # lidar node
 lidar_node = rospy.remap_name("blue1/scan")
 
-# # color ranges BGR
-# blue_ranges = {"b": {"min": 116, "max": 148}, "g": {"min": 0, "max": 4}, "r": {"min": 0, "max": 6}}
-# red_ranges = {"b": {"min": 0, "max": 17}, "g": {"min": 0, "max": 25}, "r": {"min": 128, "max": 142}}
-# green_ranges = {"b": {"min": 0, "max": 31}, "g": {"min": 243, "max": 255}, "r": {"min": 0, "max": 23}}
+# color ranges BGR
+blue_ranges = {"b": {"min": 115, "max": 255}, "g": {"min": 0, "max": 14}, "r": {"min": 0, "max": 23}}
+red_ranges = {"b": {"min": 0, "max": 7}, "g": {"min": 0, "max": 12}, "r": {"min": 85, "max": 152}}
+green_ranges = {"b": {"min": 0, "max": 6}, "g": {"min": 98, "max": 106}, "r": {"min": 0, "max": 4}}
 
-# color ranges HSV
-blue_ranges = {"H": {"min": 117, "max": 126}, "S": {"min": 235, "max": 255}, "V": {"min": 98, "max": 255}}
-red_ranges = {"H": {"min": 0, "max": 8}, "S": {"min": 171, "max": 255}, "V": {"min": 90, "max": 223}}
-green_ranges = {"H": {"min": 58, "max": 70}, "S": {"min": 104, "max": 253}, "V": {"min": 45, "max": 160}}
+
+# # color ranges HSV
+# blue_ranges = {"H": {"min": 117, "max": 126}, "S": {"min": 235, "max": 255}, "V": {"min": 98, "max": 255}}
+# red_ranges = {"H": {"min": 0, "max": 8}, "S": {"min": 171, "max": 255}, "V": {"min": 90, "max": 223}}
+# green_ranges = {"H": {"min": 58, "max": 70}, "S": {"min": 104, "max": 253}, "V": {"min": 45, "max": 160}}
 
 
 class Server:
-    def __init__(self, robot_color, show_camera_img=True):
+    def __init__(self, robot_color, show_camera_img=False):
         self.laser_data = None
         self.image_data = None
 
@@ -63,37 +64,37 @@ class Server:
             self.ranges_to_run = blue_ranges
             self.ranges_teammate = red_ranges
 
-        # # numpy arrays BGR
-        # self.lower_to_catch = np.array(
-        #     [self.ranges_to_catch['b']['min'], self.ranges_to_catch['g']['min'], self.ranges_to_catch['r']['min']])
-        # self.upper_to_catch = np.array(
-        #     [self.ranges_to_catch['b']['max'], self.ranges_to_catch['g']['max'], self.ranges_to_catch['r']['max']])
-        #
-        # self.lower_to_run = np.array(
-        #     [self.ranges_to_run['b']['min'], self.ranges_to_run['g']['min'], self.ranges_to_run['r']['min']])
-        # self.upper_to_run = np.array(
-        #     [self.ranges_to_run['b']['max'], self.ranges_to_run['g']['max'], self.ranges_to_run['r']['max']])
-        #
-        # self.lower_teammate = np.array(
-        #     [self.ranges_teammate['b']['min'], self.ranges_teammate['g']['min'], self.ranges_teammate['r']['min']])
-        # self.upper_teammate = np.array(
-        #     [self.ranges_teammate['b']['max'], self.ranges_teammate['g']['max'], self.ranges_teammate['r']['max']])
-
-        # numpy arrays HSV
+        # numpy arrays BGR
         self.lower_to_catch = np.array(
-            [self.ranges_to_catch['H']['min'], self.ranges_to_catch['S']['min'], self.ranges_to_catch['V']['min']])
+            [self.ranges_to_catch['b']['min'], self.ranges_to_catch['g']['min'], self.ranges_to_catch['r']['min']])
         self.upper_to_catch = np.array(
-            [self.ranges_to_catch['H']['max'], self.ranges_to_catch['S']['max'], self.ranges_to_catch['V']['max']])
+            [self.ranges_to_catch['b']['max'], self.ranges_to_catch['g']['max'], self.ranges_to_catch['r']['max']])
 
         self.lower_to_run = np.array(
-            [self.ranges_to_run['H']['min'], self.ranges_to_run['S']['min'], self.ranges_to_run['V']['min']])
+            [self.ranges_to_run['b']['min'], self.ranges_to_run['g']['min'], self.ranges_to_run['r']['min']])
         self.upper_to_run = np.array(
-            [self.ranges_to_run['H']['max'], self.ranges_to_run['S']['max'], self.ranges_to_run['V']['max']])
+            [self.ranges_to_run['b']['max'], self.ranges_to_run['g']['max'], self.ranges_to_run['r']['max']])
 
         self.lower_teammate = np.array(
-            [self.ranges_teammate['H']['min'], self.ranges_teammate['S']['min'], self.ranges_teammate['V']['min']])
+            [self.ranges_teammate['b']['min'], self.ranges_teammate['g']['min'], self.ranges_teammate['r']['min']])
         self.upper_teammate = np.array(
-            [self.ranges_teammate['H']['max'], self.ranges_teammate['S']['max'], self.ranges_teammate['V']['max']])
+            [self.ranges_teammate['b']['max'], self.ranges_teammate['g']['max'], self.ranges_teammate['r']['max']])
+
+        # # numpy arrays HSV
+        # self.lower_to_catch = np.array(
+        #     [self.ranges_to_catch['H']['min'], self.ranges_to_catch['S']['min'], self.ranges_to_catch['V']['min']])
+        # self.upper_to_catch = np.array(
+        #     [self.ranges_to_catch['H']['max'], self.ranges_to_catch['S']['max'], self.ranges_to_catch['V']['max']])
+        #
+        # self.lower_to_run = np.array(
+        #     [self.ranges_to_run['H']['min'], self.ranges_to_run['S']['min'], self.ranges_to_run['V']['min']])
+        # self.upper_to_run = np.array(
+        #     [self.ranges_to_run['H']['max'], self.ranges_to_run['S']['max'], self.ranges_to_run['V']['max']])
+        #
+        # self.lower_teammate = np.array(
+        #     [self.ranges_teammate['H']['min'], self.ranges_teammate['S']['min'], self.ranges_teammate['V']['min']])
+        # self.upper_teammate = np.array(
+        #     [self.ranges_teammate['H']['max'], self.ranges_teammate['S']['max'], self.ranges_teammate['V']['max']])
 
     def laser_callback(self, msg):
         self.laser_data = msg
@@ -191,17 +192,17 @@ class Server:
     def camera_navigation(self):
         # ***************** camera code *************
         img = self.bridge.imgmsg_to_cv2(self.image_data, desired_encoding='bgr8')
-        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        # hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
         # masks
-        # mask_to_catch = cv2.inRange(img, self.lower_to_catch, self.upper_to_catch)
-        # mask_to_run = cv2.inRange(img, self.lower_to_run, self.upper_to_run)
-        # mask_teammate = cv2.inRange(img, self.lower_teammate, self.upper_teammate)
+        mask_to_catch = cv2.inRange(img, self.lower_to_catch, self.upper_to_catch)
+        mask_to_run = cv2.inRange(img, self.lower_to_run, self.upper_to_run)
+        mask_teammate = cv2.inRange(img, self.lower_teammate, self.upper_teammate)
 
-        # masks
-        mask_to_catch = cv2.inRange(hsv, self.lower_to_catch, self.upper_to_catch)
-        mask_to_run = cv2.inRange(hsv, self.lower_to_run, self.upper_to_run)
-        mask_teammate = cv2.inRange(hsv, self.lower_teammate, self.upper_teammate)
+        # # masks
+        # mask_to_catch = cv2.inRange(hsv, self.lower_to_catch, self.upper_to_catch)
+        # mask_to_run = cv2.inRange(hsv, self.lower_to_run, self.upper_to_run)
+        # mask_teammate = cv2.inRange(hsv, self.lower_teammate, self.upper_teammate)
 
         # img dimensions
         height, width, dimension = img.shape
@@ -213,137 +214,118 @@ class Server:
 
         # ****************** to catch *************************
         if M_to_catch['m00'] > 0 and not self.robot_to_escape:
-            print("Robot in CATCH mode")
 
-            self.robot_to_catch = True
-
-            # edges = cv2.Canny(mask_to_catch, 100, 200)
-            edges = cv2.GaussianBlur(mask_to_catch, (7, 7), 2)
-            kernel = np.ones((7, 7), np.uint8)
-            edges = cv2.morphologyEx(edges, cv2.MORPH_OPEN, kernel)
-            edges = cv2.medianBlur(edges, 9)
-            edges = cv2.dilate(edges, kernel, iterations=4)
-            kernel = np.ones((9, 9), np.uint8)
-            edges = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel)
-
-            contours, _ = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-            # for contours in contours:
-            #     (x, y, w, h) = cv2.boundingRect(contours)
-            #     # draw the rectangle over detected object
-            #
-            #     if cv2.contourArea(contours) < 3000:
-            #         continue
-            #
-            #     cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 255), 2)
-            #     cv2.putText(edges, '+', (int(x+w/2), int(y+h/2)), cv2.FONT_ITALIC, 1, (255, 0, 0), 2, cv2.LINE_8)
-            #     # print(cv2.contourArea(contours))
+            contours, _ = cv2.findContours(mask_to_catch, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
             if len(contours) != 0:
-                # draw in blue the contours that were founded
-                # cv2.drawContours(output, contours, -1, 255, 3)
 
                 # find the biggest countour (c) by the area
                 c = max(contours, key=cv2.contourArea)
                 x, y, w, h = cv2.boundingRect(c)
 
-                # draw the biggest contour (c) in green
-                cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 3)
-                cv2.putText(edges, '+', (int(x+w/2), int(y+h/2)), cv2.FONT_ITALIC, 2, (0, 0, 255), 2, cv2.LINE_8)
+                img_noise_thresh = 1000  # this threshold prevents the robot of detecting noise in the img
 
-                cx_to_catch = int(x+w/2)
-                cy_to_catch = int(y+h/2)
-            else:
-                cx_to_catch = int(M_to_catch['m10'] / M_to_catch['m00'])
-                cy_to_catch = int(M_to_catch['m01'] / M_to_catch['m00'])
-                # cv2.circle(img, (cx_to_catch, cy_to_catch), 20, (0, 0, 255), -1)
-                cv2.putText(edges, '+', (cx_to_catch, cy_to_catch), cv2.FONT_ITALIC, 2, (0, 0, 255), 2, cv2.LINE_8)
+                if cv2.contourArea(c) > img_noise_thresh:
+                    # draw the biggest contour (c) in green
+                    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 3)
+                    # cv2.putText(edges, '+', (int(x + w / 2), int(y + h / 2)), cv2.FONT_ITALIC, 2, (0, 0, 255), 2,
+                    #             cv2.LINE_8)
 
-            # find center
-            threshold = cx_to_catch - width / 2
+                    cx_to_catch = int(x + w / 2)
+                    cy_to_catch = int(y + h / 2)
 
-            self.turn = threshold
+                    print("Robot in CATCH mode")
 
-            if abs(self.turn) < 150:  # accelerate
-                if self.speed < 1.0:
-                    self.speed += 0.1
-            else:
-                if self.speed > 0.3:  # decelerate
-                    self.speed -= 0.05
-                else:
-                    self.speed = 0.3
+                    self.robot_to_catch = True
+
+                    # find center
+                    threshold = cx_to_catch - width / 2
+
+                    self.turn = threshold
+
+                    if abs(self.turn) < 150:  # accelerate
+                        if self.speed < 1.0:
+                            self.speed += 0.1
+                    else:
+                        if self.speed > 0.3:  # decelerate
+                            self.speed -= 0.05
+                        else:
+                            self.speed = 0.3
+
+                elif not self.robot_to_escape:  # not detected target
+                    self.robot_to_catch = False
 
         elif not self.robot_to_escape:  # not detected target
             self.robot_to_catch = False
 
         # ****************** to escape *************************
         if M_to_run['m00'] > 0:
-            print("Robot in ESCAPE mode")
 
             cx_to_run = int(M_to_run['m10'] / M_to_run['m00'])
             cy_to_run = int(M_to_run['m01'] / M_to_run['m00'])
             # cv2.circle(img, (cx_to_run, cy_to_run), 20, (255, 0, 0), -1)
 
-            # edges = cv2.Canny(mask_to_run, 100, 200)
-            edges = cv2.GaussianBlur(mask_to_run, (7, 7), 2)
-            kernel = np.ones((7, 7), np.uint8)
-            edges = cv2.morphologyEx(edges, cv2.MORPH_OPEN, kernel)
-            edges = cv2.medianBlur(edges, 9)
-            edges = cv2.dilate(edges, kernel, iterations=4)
-            kernel = np.ones((9, 9), np.uint8)
-            edges = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel)
-
-            contours, _ = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            contours, _ = cv2.findContours(mask_to_run, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
             if len(contours) != 0:
 
                 c = max(contours, key=cv2.contourArea)
                 x, y, w, h = cv2.boundingRect(c)
 
-                cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 3)
-                cv2.putText(edges, '+', (int(x+w/2), int(y+h/2)), cv2.FONT_ITALIC, 2, (0, 255, 0), 2, cv2.LINE_8)
+                img_noise_thresh = 1000  # this threshold prevents the robot of detecting the green arena
 
-            # escape from the robot
-            self.robot_to_escape = True
-            self.speed, self.turn = self.lidar_navigation(lidar_max_dist_to_obj=1.5, lidar_turn_speed=350)
+                if cv2.contourArea(c) > img_noise_thresh:
+                    print("Robot in ESCAPE mode")
+
+                    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 3)
+                    # cv2.putText(edges, '+', (int(x + w / 2), int(y + h / 2)), cv2.FONT_ITALIC, 2, (0, 255, 0), 2,
+                    #             cv2.LINE_8)
+
+                    # escape from the robot
+                    self.robot_to_escape = True
+                    self.speed, self.turn = self.lidar_navigation(lidar_max_dist_to_obj=1.5, lidar_turn_speed=350)
+
+                else:
+                    self.robot_to_escape = False
 
         else:
             self.robot_to_escape = False
-            
+            # print(self.robot_to_escape)
+
         # ********************** team mate found *******************
         if M_teammate['m00'] > 0 and not self.robot_to_escape and not self.robot_to_catch:
-            print("Team mate found")
 
             cx_teammate = int(M_teammate['m10'] / M_teammate['m00'])
             cy_teammate = int(M_teammate['m01'] / M_teammate['m00'])
             # cv2.circle(img, (cx_teammate, cy_teammate), 20, (255, 0, 0), -1)
 
-            # edges = cv2.Canny(mask_teammate, 100, 200)
-            edges = cv2.GaussianBlur(mask_teammate, (7, 7), 2)
-            kernel = np.ones((7, 7), np.uint8)
-            edges = cv2.morphologyEx(edges, cv2.MORPH_OPEN, kernel)
-            edges = cv2.medianBlur(edges, 9)
-            edges = cv2.dilate(edges, kernel, iterations=4)
-            kernel = np.ones((9, 9), np.uint8)
-            edges = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel)
-
-            contours, _ = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            contours, _ = cv2.findContours(mask_teammate, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
             if len(contours) != 0:
 
                 c = max(contours, key=cv2.contourArea)
                 x, y, w, h = cv2.boundingRect(c)
 
-                cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 3)
-                cv2.putText(edges, '+', (int(x+w/2), int(y+h/2)), cv2.FONT_ITALIC, 2, (255, 0, 0), 2, cv2.LINE_8)
+                img_noise_thresh = 1000  # this threshold prevents the robot of detecting the green arena
 
-            # keep teammates apart from each other
-            self.robot_teammate = True
-            self.turn = 550
-            self.speed = 0
+                if cv2.contourArea(c) > img_noise_thresh:
+                    # keep teammates apart from each other
+                    print("Team mate found")
+
+                    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 3)
+                    # cv2.putText(edges, '+', (int(x + w / 2), int(y + h / 2)), cv2.FONT_ITALIC, 2, (255, 0, 0), 2,
+                    #             cv2.LINE_8)
+
+                    self.robot_teammate = True
+                    self.turn = 550
+                    self.speed = 0
+
+                else:
+                    self.robot_teammate = False
+
         else:
             self.robot_teammate = False
-            
+
         if self.show_camera_img:
             # resize
             img = cv2.resize(img, (300, 300))
