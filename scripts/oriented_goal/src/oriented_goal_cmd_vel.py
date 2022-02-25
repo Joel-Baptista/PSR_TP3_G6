561f7918e0d4f20aef82210f26d9517252f1e13e
jorge <jorge.andre.silva.fernandes@gmail.com>
2022-02-25 15:58:21 +0000

    update on oriented goal - detecting teammate

--- scripts/oriented_goal/src/oriented_goal_cmd_vel.py
+++ scripts/oriented_goal/src/oriented_goal_cmd_vel.py
@@ -20,10 +20,13 @@ angle = 0  # robot angle to turn
 
 # flags
 obj_detected = False  # flag for lidar detection
+robot_teammate_near = False
 robot_to_escape_near = False  # flag robot to escape detected
 robot_to_catch_near = False  # flag robot to catch near
 
 # list to store positions
+robot_teammate_poses_x = []
+robot_teammate_poses_y = []
 robot_to_catch_poses_x = []
 robot_to_catch_poses_y = []
 robot_to_catch_ori_z = []
@@ -32,6 +35,7 @@ robot_to_escape_poses_y = []
 robot_to_escape_ori_z = []
 
 # list to store distances
+distances_teammates = []
 distances_to_catch = []
 distances_to_escape = []
 
@@ -51,7 +55,7 @@ pub_cmd_vel = rospy.Publisher(cmd_vel_node, Twist, queue_size=10)
 
 
 def laser_callback(laser_data):
-    global speed, angle, obj_detected, robot_to_escape_near, robot_to_catch_near
+    global speed, angle, obj_detected, robot_to_escape_near, robot_to_catch_near, robot_teammate_near
 
     regions = {
         # lidar front ranges
@@ -185,6 +189,89 @@ def laser_callback(laser_data):
         twist.angular.z = angle
         pub_cmd_vel.publish(twist)
 
+    elif not robot_to_escape_near and not robot_to_catch_near and robot_teammate_near:
+
+        lidar_max_dist_to_obj = 1.5
+        lidar_angle_speed = 350
+
+        if regions["frontr"] < lidar_max_dist_to_obj or regions["frontl"] < lidar_max_dist_to_obj:
+            speed = 0
+            obj_detected = True
+
+            # this is used when the robot as only blocking objects in front of them
+            # otherwise it will stop moving
+            if regions["right"] <= regions["left"]:
+                angle = -lidar_angle_speed
+            else:
+                angle = lidar_angle_speed
+            # **************************************
+
+            # make the robot go backwards when it is too close to an object
+            if regions["frontr"] < 0.2 or regions["frontl"] < 0.2:
+                speed = -0.3
+            # ******************************
+
+            # angle decisions ********************
+            if regions["right"] < lidar_max_dist_to_obj:
+                angle = -lidar_angle_speed
+
+            elif regions["left"] < lidar_max_dist_to_obj:
+                angle = lidar_angle_speed
+            # **********************
+
+            angle = -float(angle) / 500
+            print("teammate detected in front")
+
+            # pub on cmd_vel
+            twist.linear.x = speed
+            twist.angular.z = angle
+            pub_cmd_vel.publish(twist)
+
+        elif regions['right'] < lidar_max_dist_to_obj and regions["frontr"] > lidar_max_dist_to_obj and regions[
+            "frontl"] > lidar_max_dist_to_obj and regions['left'] > lidar_max_dist_to_obj:
+            speed = 0.5
+            angle = 0
+            obj_detected = True
+
+            print("teammate detected on the right")
+
+            # pub on cmd_vel
+            twist.linear.x = speed
+            twist.angular.z = angle
+            pub_cmd_vel.publish(twist)
+
+        elif regions['right'] > lidar_max_dist_to_obj and regions["frontr"] > lidar_max_dist_to_obj and regions[
+            "frontl"] > lidar_max_dist_to_obj and regions['left'] < lidar_max_dist_to_obj:
+            speed = 0.5
+            angle = 0
+            obj_detected = True
+
+            print("teammate detected on the left")
+
+            # pub on cmd_vel
+            twist.linear.x = speed
+            twist.angular.z = angle
+            pub_cmd_vel.publish(twist)
+
+        elif regions['right'] < lidar_max_dist_to_obj and regions['left'] < lidar_max_dist_to_obj:
+            speed = 0.5
+            angle = 0
+            obj_detected = True
+
+            print("teammate detected on the sides")
+
+            # pub on cmd_vel
+            twist.linear.x = speed
+            twist.angular.z = angle
+            pub_cmd_vel.publish(twist)
+
+        elif regions['right'] > lidar_max_dist_to_obj and regions['frontr'] > lidar_max_dist_to_obj and regions[
+            'frontl'] > lidar_max_dist_to_obj and regions['left'] > lidar_max_dist_to_obj:
+            speed = speed
+            angle = angle
+            obj_detected = False
+
+
     else:
         speed = speed
         angle = angle
@@ -196,12 +283,14 @@ def main():
     Initialization *****************************
     """
 
-    global speed, angle, robot_to_escape_near, robot_to_catch_near
+    global speed, angle, robot_to_escape_near, robot_to_catch_near, robot_teammate_near
 
     rospy.init_node('get_robot_position')  # init node
     rate = rospy.Rate(rate_hz)  # rate to sleep
 
     robot_name = rospy.get_param("~robot_name", default="blue1")  # robot player
+    robot_teammate_2 = rospy.get_param("~robot_teammate_2", default="blue2")  # robot teammate
+    robot_teammate_3 = rospy.get_param("~robot_teammate_3", default="blue3")  # robot teammate
 
     robot_color = rospy.get_param("~robot_color", default="Blue")  # robot color param
 
@@ -217,6 +306,7 @@ def main():
     rospy.wait_for_service(get_model_state)
     g_get_state = rospy.ServiceProxy(get_model_state, GetModelState)
 
+    goal_teammate = PointStamped()
     goal_to_catch = PointStamped()
     goal_to_escape = PointStamped()
 
@@ -230,6 +320,8 @@ def main():
 
     while not rospy.is_shutdown():
         # reset the lists to store new position
+        robot_teammate_poses_x.clear()
+        robot_teammate_poses_y.clear()
         robot_to_catch_poses_x.clear()
         robot_to_catch_poses_y.clear()
         robot_to_escape_poses_x.clear()
@@ -240,6 +332,8 @@ def main():
 
         # get robot positions
         state_my_pos = g_get_state(model_name=robot_name)
+        state_teammate_2 = g_get_state(model_name=robot_teammate_2)
+        state_teammate_3 = g_get_state(model_name=robot_teammate_3)
 
         # to catch positions
         state_to_catch_1 = g_get_state(model_name=robot_to_catch_1)
@@ -258,6 +352,22 @@ def main():
         y_pos = state_my_pos.pose.position.y
         z_pos = state_my_pos.pose.orientation.z
         # print(x_pos, y_pos)
+
+        # teammates positions **********************************
+        x_teammate_2 = state_teammate_2.pose.position.x
+        y_teammate_2 = state_teammate_2.pose.position.y
+        x_teammate_3 = state_teammate_3.pose.position.x
+        y_teammate_3 = state_teammate_3.pose.position.y
+
+        robot_teammate_poses_x.append(x_teammate_2)
+        robot_teammate_poses_x.append(x_teammate_3)
+        robot_teammate_poses_y.append(y_teammate_2)
+        robot_teammate_poses_y.append(y_teammate_3)
+
+        distance_teammate_2 = math.sqrt((x_teammate_2 - x_pos) ** 2 + (y_teammate_2 - y_pos) ** 2)
+        distance_teammate_3 = math.sqrt((x_teammate_3 - x_pos) ** 2 + (y_teammate_3 - y_pos) ** 2)
+        distances_teammates.append(distance_teammate_2)
+        distances_teammates.append(distance_teammate_3)
 
         # position of targets to catch **************************
         x_to_catch_1 = state_to_catch_1.pose.position.x
@@ -317,6 +427,7 @@ def main():
         distances_to_escape.append(distance_to_escape_2)
         distances_to_escape.append(distance_to_escape_3)
 
+        thresh_dist_teammate = 1  # max proximity to teammate to avoid collision
         thresh_dist_to_escape = 2  # max proximity from robot to escape
         thresh_dist_to_catch = 1  # proximity from robot to catch
 
@@ -330,6 +441,12 @@ def main():
             robot_to_catch_near = True
         else:
             robot_to_catch_near = False
+
+        # flag to detect teammate
+        if distance_teammate_2 < thresh_dist_teammate or distance_teammate_3 < thresh_dist_teammate:
+            robot_teammate_near = True
+        else:
+            robot_teammate_near = False
 
         # catch mode ********************************
         if not robot_to_escape_near:
@@ -349,7 +466,7 @@ def main():
 
             # print(goal_to_catch)
 
-            # create a pointstamp position equal to the robot to catch position to fed intro tf transformation
+            # create a pointstamp position equal to the robot to catch position to fed into tf transformation
             goal_to_catch_present_time = PointStamped()
             goal_to_catch_present_time.header.stamp = rospy.Time(0)
             goal_to_catch_present_time.header.frame_id = robot_name + "/odom"
@@ -400,8 +517,71 @@ def main():
 
             # print robot to catch position
             print(
-                f'Red at position {round(goal_to_catch.pose.position.x, 1)},{round(goal_to_catch.pose.position.y, 1)}. Moving at speed:{round(speed, 1)}, angle:{round(angle, 1)}')
+                f'Robot to catch at position {round(goal_to_catch.pose.position.x, 1)},{round(goal_to_catch.pose.position.y, 1)}. Moving at speed:{round(speed, 1)}, angle:{round(angle, 1)}')
 
+        elif not robot_to_escape_near and not robot_to_catch_near and robot_teammate_near:
+            # robot teammate near robot player *******************
+            
+            min_dist_teammate = min(distances_teammates)
+            
+            # decide what robot to catch (the closer one)
+            if min_dist_teammate == distances_teammates[0]:
+                goal_teammate = state_teammate_2
+            if min_dist_teammate == distances_teammates[1]:
+                goal_teammate = state_teammate_3
+                
+            # create a pointstamp position equal to the robot teammate position to fed into tf transformation
+            goal_teammate_present_time = PointStamped()
+            goal_teammate_present_time.header.stamp = rospy.Time(0)
+            goal_teammate_present_time.header.frame_id = robot_name + "/odom"
+            goal_teammate_present_time.point.x = goal_teammate.pose.position.x
+            goal_teammate_present_time.point.y = goal_teammate.pose.position.y
+            goal_teammate_present_time.point.z = goal_teammate.pose.position.z
+            # print(goal_teammate_present_time)
+
+            # create tf transformation
+            target_frame = robot_name + "/base_link"  # frame to transform final position
+            # print(target_frame)
+
+            goal_in_base_link_teammate = tf_buffer.transform(goal_teammate_present_time, target_frame,
+                                                              rospy.Duration(1))
+            # print(goal_in_base_link_teammate)
+
+            x_teammate = goal_teammate.pose.position.x
+            y_teammate = goal_teammate.pose.position.y
+
+            # go to safe position
+            x = goal_in_base_link_teammate.point.x
+            y = goal_in_base_link_teammate.point.y
+            orientation = goal_in_base_link_teammate.point.z
+
+            if not obj_detected:
+                if abs(orientation) > 0.5:  # decelerate and turn to face target
+                    angle = math.atan2(y, x)
+                    if speed >= 0.5:
+                        speed -= 0.15
+                    else:
+                        speed = 0.5
+                else:
+                    angle = math.atan2(y, x)
+
+                    if abs(angle) < 0.20:  # accelerate
+                        if speed < max_speed:
+                            speed += 0.1
+                    else:
+                        speed = 0.5
+
+                # pub on cmd_vel
+                twist.linear.x = speed
+                twist.angular.z = angle
+                pub_cmd_vel.publish(twist)
+                print("no teammate detected")
+
+            # lidar code *********************************************
+            rospy.Subscriber(lidar_node, LaserScan, laser_callback)
+            
+            print(f'Teammate at position {round(goal_teammate.pose.position.x, 1)},{round(goal_teammate.pose.position.y)}')
+            
         # escape mode ******************************************
         else:
             # final pose to escape *****************************
@@ -418,7 +598,7 @@ def main():
 
             # print(goal_to_escape)
 
-            # create a pointstamp position equal to the robot to escape position to fed intro tf transformation
+            # create a pointstamp position equal to the robot to escape position to fed into tf transformation
             goal_to_escape_present_time = PointStamped()
             goal_to_escape_present_time.header.stamp = rospy.Time(0)
             goal_to_escape_present_time.header.frame_id = robot_name + "/odom"
@@ -466,7 +646,7 @@ def main():
             if y < y_arena_min:
                 y = y_arena_min
             # ******************************************************************
-            if obj_detected:
+            if not obj_detected:
                 if abs(orientation) > 0.5:  # decelerate and turn to face target
                     angle = math.atan2(y, x)
                     if speed >= 0:
@@ -492,7 +672,7 @@ def main():
             rospy.Subscriber(lidar_node, LaserScan, laser_callback)
 
             print(
-                f'Green at position {round(x_to_escape, 1)},{round(y_to_escape, 1)} go to position {round(x, 1)}, {round(y, 1)} at speed:{round(speed, 1)}, angle:{round(angle, 1)}')
+                f'Robot to escape at position {round(x_to_escape, 1)},{round(y_to_escape, 1)} go to position {round(x, 1)}, {round(y, 1)} at speed:{round(speed, 1)}, angle:{round(angle, 1)}')
 
         rate.sleep()
 
